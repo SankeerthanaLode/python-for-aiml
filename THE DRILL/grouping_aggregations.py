22624c5009986082430d762519f322218a26b4cc
Sankeerthana1745 <sankeerthanalode@gmail.com>
2026-05-12 08:49:51 +0000

    added the file grouping_aggregations.py

---
+++ THE DRILL/grouping_aggregations.py
@@ -0,0 +1,12 @@
+import pandas as pd
+
+data = {
+    "name": ["A", "B", "C", "D", "E"],
+    "marks": [85, 90, 78, 92, 88],
+    "city": ["Hyd", "Hyd", "Delhi", "Delhi", "Hyd"]
+}
+
+df = pd.DataFrame(data)
+
+print(df.groupby("city")["marks"].agg(["mean", "max"]))
+
